86e180d2d8530670817ad53c81fff38d6b77b2ef
Charlemagne Hernani <chlemagne@gmail.com>
2020-11-10 09:14:55 +0800

    Create Dynamic Integer Array using linear algorithms

---
+++ src/com/chlemagne/mystructures/DynamicIntArray.java
@@ -0,0 +1,95 @@
+package com.chlemagne.mystructures;
+
+
+import java.util.Arrays;
+
+/**
+ * Custom Dynamic Integer Array using linear algorithms.
+ */
+public class DynamicIntArray {
+
+    private int insertPointer;
+    private int[] items;
+
+    /**
+     * Create dynamic int array of fixed length.
+     * @param length
+     */
+    public DynamicIntArray(int length) {
+        this.insertPointer = 0;
+        this.items = new int[length];
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(items, 0, insertPointer));
+    }
+
+    /**
+     * Insert item from the tail of the array.
+     * @param item
+     */
+    public void insert(int item) {
+        // check size
+        if (insertPointer >= items.length)
+            doubleArraySize();
+
+        items[insertPointer++] = item;
+    }
+
+    /**
+     * Remove item at index. As result, items to the right of index will be shifted to the left
+     * and the new size of the array is one less.
+     * @param index
+     */
+    public void removeAt(int index) {
+        /*
+            Scenario 1:
+                count = 321 (after insert)
+                index = 320
+         */
+        // validate index
+        if (index < 0 || index >= insertPointer)
+            throw new IndexOutOfBoundsException();
+
+        // linear algorithm
+        for (int i = index; i < insertPointer; i++) {
+            /* Shift-left either:
+                a.) items[1 + 1] or right adjacent item
+                b.) zero (0) if index is last item */
+            items[i] = (index == insertPointer - 1) ? 0 : items[i + 1]; // shift-left
+        }
+
+        this.insertPointer--;
+    }
+
+    /**
+     * Find the first match of item and return its index.
+     * @param item
+     * @return Index of item if found or -1.
+     */
+    public int indexOf(int item) {
+
+        // linear search algorithm
+        for (int i = 0; i < insertPointer; i++) {
+            if (item == items[i])
+                return i;
+        }
+
+        return -1;
+    }
+
+    /**
+     * Resize array size by one. Items in the original array are linearly copied to the new array.
+     */
+    private void doubleArraySize() {
+
+        int[] temp = new int[items.length * 2]; // double the length of new array
+
+        // linear copy algorithm
+        for (int i = 0; i < items.length; i++)
+            temp[i] = items[i]; // copy item in original array to the new array
+
+        items = temp; // discard original array
+    }
+}
---
+++ src/com/chlemagne/mystructures/DynamicIntArrayTest.java
@@ -0,0 +1,37 @@
+package com.chlemagne.mystructures;
+
+import java.util.Arrays;
+
+public class DynamicIntArrayTest {
+
+    public static void main (String[] args) {
+
+        DynamicIntArray array = new DynamicIntArray(3);
+        System.out.printf("dbg_a: %s%n", array);
+
+        array.insert(10);
+        array.insert(20);
+        array.insert(30);
+        System.out.printf("dbg_b: %s%n", array);
+        array.indexOf(100);
+
+        array.insert(40);
+        System.out.printf("dbg_c: %s%n", array);
+
+        System.out.printf("dbg_d: %s%n", array.indexOf(30));
+
+        array.removeAt(1);
+        System.out.printf("dbg_e: %s%n", array.indexOf(30));
+        System.out.printf("dbg_f: %s%n", array);
+
+        array.insert(50);
+        array.insert(60);
+        array.insert(70);
+        System.out.printf("dbg_g: %s%n", array);
+        array.removeAt(array.indexOf(70));
+        System.out.printf("dbg_h: %s%n", array);
+
+        array.insert(80);
+        System.out.printf("dbg_i: %s%n", array);
+    }
+}
